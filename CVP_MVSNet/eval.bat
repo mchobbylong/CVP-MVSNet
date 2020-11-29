d052e64aa817556bf409c4f909e1a17a7fb4f00c
Michael Long <mchobbylong@gmail.com>
2020-11-29 21:49:36 +0800

    update eval.bat

--- CVP_MVSNet/eval.bat
+++ CVP_MVSNet/eval.bat
@@ -1,23 +1,25 @@
 @rem Dataset
-@set DATASET_ROOT="./dataset/dtu-test-1200/"
+@set dataset_name=dtu-test-1200
+@set dataset_root=./dataset
+@set dataset_path=%dataset_root%/%dataset_name%/
+@set output_path=outputs/%dataset_name%/
 
 @rem Checkpoint
-@set LOAD_CKPT_DIR="./checkpoints/pretrained/model_000027.ckpt"
+@set CKPT_PATH="./checkpoints/pretrained/model_000027.ckpt"
 
 @rem Logging
-@set LOG_DIR="./logs/"
+@set LOG_DIR=./logs/
 
-@rem Output dir
-@set OUT_DIR="./outputs_pretrained/"
+@set CUDA_LAUNCH_BLOCKING=1
 
 python eval.py ^
 --info="eval_pretrained_e27" ^
 --mode="test" ^
---dataset_root=%DATASET_ROOT% ^
+--dataset_root=%dataset_path% ^
 --imgsize=1200 ^
 --nsrc=4 ^
 --nscale=5 ^
 --batch_size=1 ^
---loadckpt=%LOAD_CKPT_DIR% ^
+--loadckpt=%CKPT_PATH% ^
 --loggingdir=%LOG_DIR% ^
---outdir=%OUT_DIR%
+--outdir=%output_path%
